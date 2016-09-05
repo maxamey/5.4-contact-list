a5d2e6609a868e7af2a1d52ea0f139a810cb911a
Max Amey <maxwellamey@gmail.com>
2016-09-05 11:54:54 -0500

    Added info into the list on the single contact view

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Main from './lib/main'
+import { Router, Route, hashHistory } from 'react-router'
+import Main from './lib/Main'
+import Contact from './lib/Contact'
 
 render((
-  <Main name="Boilerplate"/>
+  <Router history={ hashHistory }>
+    <Route path="/"
+           component={ Main } />
+    <Route path="/contact/:namef/:namel/:image/:email/:phone/:location"
+    component={ Contact } />
+  </Router>
 ), document.getElementById('app'))
