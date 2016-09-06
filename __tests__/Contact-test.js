cf4ecb6effbd9312a7b5e07f79474d918a9c0a90
Max Amey <maxwellamey@gmail.com>
2016-09-06 13:22:21 -0500

    Added a dependecy that I needed to run the test for main.js

--- __tests__/Contact-test.js
+++ __tests__/Contact-test.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import Contact from '../lib/Contact';
 
-jest.unmock('../lib/Contact');
 
 describe("Contact View", () => {
-  
+
 })
--- __tests__/Main-test.js
+++ __tests__/Main-test.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import Main from '../lib/Main';
 
-jest.unmock('../lib/Main');
 
 describe("Main List of Contacts", () => {
   const main = TestUtils.renderIntoDocument(
     <Main />
   );
-  const mainEl = ReactDOM.findDOMNode(main);
-  expect(this.props.contatcs.length).toEqual(8);
-
+  it("should have eight contacts",() => {    expect(main.props.contacts.length).toEqual(8);
+  })
 });
